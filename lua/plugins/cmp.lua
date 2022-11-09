f84623e5c9fc78204967cbd7bf93cfd1f3100959
Artur Gomes <contato@arturgomes.com.br>
2022-11-09 15:40:19 -0300

    Remove the worst bind ever

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -58,7 +58,7 @@ cmp.setup({
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		-- ["<Right>"] = cmp.mapping.confirm { select = true },
+		-- ["<Right>"] = cmp.mapping.confirm { select = true }, -- This is the worst thing ever
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
